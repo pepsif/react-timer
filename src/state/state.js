aca2c7770c54a7511cd893a53239e332c59670f0
pepsif <pepsi_fack@ukr.net>
2022-11-03 20:54:17 +0200

    feat: add zero number before seconds count

--- src/state/state.js
+++ src/state/state.js
@@ -7,32 +7,45 @@ const state = {
   seconds: "00",
   t: "",
 
-  tick() {
-    this.seconds++
-  },
-  startTimer() {
-    if (this.starter === "play") { 
-         return
-    }
-  
-   this.starter = "play"
 
-   this.t = setInterval( () => {
-    this.tick()
-    rerenderTree()
-      },1000)
+  startTimer() {
+    if (this.starter === "play") {
+      return
+    }
+    this.starter = "play"
+
+    this.t = setInterval(() => {
+      
+      this.tick()
+      rerenderTree()
+    }, 1000)
   },
   stopTimer() {
     clearInterval(this.t)
     this.starter = "stop"
   },
-  resetTimer(){
+  resetTimer() {
     this.hours = "00"
     this.minutes = "00"
     this.seconds = "00"
     rerenderTree()
+  },
+  tick() {
+     this.seconds++
+    // if ( this.minutes < 10) {
+    //  this.minutes = "0" + this.minutes
+
+    // } 
+    if (this.seconds < 10) {
+      this.seconds = "0" + this.seconds
+    }
+   if ( this.seconds === 60 ) {
+    this.minutes++
+    this.seconds = 0
+   }
+
+
   }
-  
 
 }
 // clearInterval(state.t)
