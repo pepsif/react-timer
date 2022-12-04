05d807f01ea78afe3d1675c0ef7f3db6a5572719
pepsif <pepsi_fack@ukr.net>
2022-12-04 21:05:33 +0200

    feat: repair timer terms

--- src/components/clock/clock.jsx
+++ src/components/clock/clock.jsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, {createRef} from "react";
+
 import './clock.css'
 
 function Clock(props) {
-  return (
-    <section className="clock-block">
-      <div className="container clock-container">
-        <div className="clock-table">
-          <div className="number-block">{props.state.clockHours}:{props.state.clockMinutes}</div>
-          <div className="alarm-block">
-            <img className="bell-icon" src={require("../../images/bell.jpg")} alt="bell-icon"></img>
-            <span className="alarm-day">{props.state.currentDay}</span>
-          </div>
-        </div>
-      </div>
+    const bellIcon = createRef()
+    const settingsModal = createRef()
 
-    </section>
-  )
+    let clockSettingsWindow = {
+        count: 0,
+        color: 'red',
+        display: 'none',
+        openSettingsWindow() {
+            clockSettingsWindow.count += 1
+            clockSettingsWindow.display = "block"
+            console.log(clockSettingsWindow.count)
+        }
+
+    }
+    const openModal = () => {
+        clockSettingsWindow.openSettingsWindow()
+    }
+
+    return (
+        <section className="clock-block">
+            <div className="container clock-container">
+                <div className="clock-table">
+                    <div className="number-block">{props.state.clockHours}:{props.state.clockMinutes}</div>
+                    <div className="alarm-block">
+                        <img className="bell-icon" src={require("../../images/bell.jpg")} alt="bell-icon"
+                             onClick={openModal} ref={bellIcon}></img>
+                        <span className="alarm-day">{props.state.currentDay}</span>
+                        <img className="settings-icon" src={require("../../images/settings.png")}
+                             alt="settings icon"></img>
+                    </div>
+                    <div className="clock-settings-modal" style={clockSettingsWindow} ref={settingsModal}>
+                        <h3 className="clock-settings-title">set alarm</h3>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    )
+
 }
+
 
 export default Clock
--- src/state/timer-state.js
+++ src/state/timer-state.js
@@ -2,10 +2,6 @@ import timerEndBeep from "../beep/Beep-09.ogg"
 import rerenderTree from "../index";
 const timerEnd = new Audio(timerEndBeep);
 
-
-
-
-
 const timerState = {
     timerStarter: false,
     timerMinutes: 0,
@@ -14,7 +10,11 @@ const timerState = {
     timerDisplay: "00:00",
 
     timerStart() {
-        if (this.timerStarter === true) return;
+        if (this.timerStarter === false && this.timerSeconds === 0 && this.timerMinutes === 0) {
+            return
+        }
+        if (this.timerStarter === true ) return;
+        
         this.timerStarter = true //first init starter
         this.timerMinutes -= 1   // first init minutes
         this.timerSeconds = 60
@@ -24,8 +24,11 @@ const timerState = {
             if (this.timerSeconds === 0 && this.timerMinutes === 0) {
                 clearInterval(this.timerInterval)
                 this.timerStarter = false
-                this.timerSeconds = "00"
+                this.timerSeconds = 0
+                this.timerMinutes = 0
+                this.timerDisplay = "00:00"
                 timerEnd.play()
+                return
             }
 
             if (this.timerSeconds === 0) {
@@ -50,7 +53,7 @@ const timerState = {
         if (this.timerMinutes < 10) {
             this.timerDisplay = "0" + `${this.timerMinutes}: 0${this.timerSeconds}`
         }
-        // rerenderTree()
+      
     },
     timerDecrease() {
         if (this.timerMinutes === 0) return
@@ -59,7 +62,7 @@ const timerState = {
         if (this.timerMinutes < 10) {
             this.timerDisplay = "0" + `${this.timerMinutes}: 0${this.timerSeconds}`
         }
-        // rerenderTree()
+     
     }
 }
 
