c1da2ddba35abbd4a1a0754357ed16b6a7d4dbdd
pepsif <pepsi_fack@ukr.net>
2023-02-03 14:18:47 +0300

    add: timerStart method in Timer.jsx

--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -2,60 +2,53 @@ import React from "react";
 import "./timer.css";
 
 const Timer = () => {
-  const [timer, setTimer] = React.useState(0);
+  const [timerMinute, setTimerMinute] = React.useState(0);
+  const [ timerSecond, setTimerSecond] = React.useState(59)
+  let timerStarter = false;
+  let timerInterval = 't';
   let timerMinutes = 0;
-  let timerSeconds = 59;
-  let timerDisplay = `${timer} : ${timerSeconds}`;
+  // let timerSeconds = 59;
+  let timerDisplay = `${timerMinute} : ${timerSecond}`;
 
   const timerIncrease = () => {
-    setTimer(timer + 1);
-    timerMinutes = timer;
+    setTimerMinute(timerMinute + 1);
+    
   };
 
   const timerDecrease = () => {
-    if (timer === 0) return;
-    setTimer(timer - 1);
-    timerMinutes = timer;
+    if (timerMinute === 0) return;
+    setTimerMinute(timerMinute - 1);
+    timerMinutes = timerMinute;
   };
-  const timerStart = () => {};
+  const timerStart = () => {
+    if(timerStarter === true) return
+    timerStarter = true;
+    timerInterval = setInterval(()=>{
+    
+      // setTimerSecond(timerSecond - 1)
+      console.log(timerSecond)
+    },1000)
+  };
 
   return (
     <section className="timer-section">
-      <span style={{ backgroundColor: "red" }}>{timerDisplay}</span>
+      <span style={{ backgroundColor: "red",float: "right" }}>{timerDisplay}</span>
       <div className="container timer-container">
         <div className="timer-block">
           <h3 className="timer-title">timer</h3>
           <div className="timer-button-block">
-            <button
-              className="button up"
-              onClick={() => {
-                timerIncrease();
-              }}
-            >
-              +
-            </button>
-            <button
-              className="button start"
-              onClick={() => {
-                timerStart();
-              }}
-            >
-              start
-            </button>
-            <button
-              className="button down"
-              onClick={() => {
-                timerDecrease();
-              }}
-            >
-              -
-            </button>
-          </div>
-          <span className="timer-count"> {timer<10 ? "0"+ timer : timer} : 00</span>
+            <button className="button up"onClick={() => {timerIncrease()}}>+</button>
+            <button className="button start" onClick={() => {timerStart()}}>start</button>
+            <button className="button down" onClick={() => { timerDecrease(); }}>-</button>
+           </div>   
+           <span className="timer-count"> {timerMinute<10 ? "0"+ timerMinute : timerMinute} : { timerSecond}</span> 
         </div>
-      </div>
-    </section>
-  );
-};
+            
+      </div>        
+    </section>        
+   )
+}             
+             
+
 
 export default Timer;
