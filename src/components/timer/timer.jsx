d38c7a5ea9c3ee9ae95f98df5bbb7de69c63535d
pepsif <pepsi_fack@ukr.net>
2023-02-10 18:18:10 +0300

    add: edit Timer.js

--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -2,58 +2,87 @@ import React from "react";
 import "./timer.css";
 
 const Timer = () => {
- 
   const [timerMinute, setTimerMinute] = React.useState(0);
-  const [ timerSecond, setTimerSecond] = React.useState(59)
-  let timerStarter = false;
-  let timerInterval = '';
-    console.log(timerInterval)
-   const timerIncrease = () => {
+  const [timerSecond, setTimerSecond] = React.useState(0);
+  const [timerStarter, setTimerStarter] = React.useState(false);
+
+  let timerInterval = "";
+  console.log(timerInterval);
+  const timerIncrease = () => {
     setTimerMinute(timerMinute + 1);
-    
   };
 
   const timerDecrease = () => {
     if (timerMinute === 0) return;
     setTimerMinute(timerMinute - 1);
-    };
+  };
   const timerStart = () => {
-    
-    if(timerStarter === true) return
-     timerStarter = true;
-      
-     timerInterval = setInterval(() => {
-     
-      setTimerSecond(timerSecond => {
-          // console.log(timerSecond)
+    if (timerMinute === 0 && timerSecond === 0 && timerStarter === false)
+      return;
+    if (timerStarter === true) return;
+    setTimerStarter(true);
+    setTimerSecond(59);
+    setTimerMinute((timerMinute) => timerMinute - 1);
 
-        if(timerSecond === 0) return "huy"
-       return timerSecond - 1
-      }
-        )
-      
-    },1000)
+    timerInterval = setInterval(() => {
+      setTimerSecond( (timerSecond) => {
+        timerSecond-1
+        // if (timerMinute === 0 && timerSecond === 0) {
+        //   setTimerStarter(false);
+        //   clearInterval(timerInterval);
+        //   setTimerSecond(0);
+        //   return;
+        // }
+
+        // if (timerMinute > 0 && timerSecond === 1) {
+        //   setTimerMinute((timerMinute) => timerMinute - 1);
+        //   setTimerSecond(0);
+        // }
+        
+      });
+    }, 1000);
   };
 
   return (
     <section className="timer-section">
-      <span style={{ backgroundColor: "red",float: "right",fontSize:"2rem" }}>{timerMinute}:{timerSecond}</span>
       <div className="container timer-container">
         <div className="timer-block">
           <h3 className="timer-title">timer</h3>
           <div className="timer-button-block">
-            <button className="button up"onClick={() => {timerIncrease()}}>+</button>
-            <button className="button start" onClick={() => {timerStart()}}>start</button>
-            <button className="button down" onClick={() => { timerDecrease(); }}>-</button>
-           </div>   
-           <span className="timer-count"> {timerMinute<10 ? "0"+ timerMinute : timerMinute} : { timerSecond < 10 ? "0" + timerSecond : timerSecond}</span> 
+            <button
+              className="button up"
+              onClick={() => {
+                timerIncrease();
+              }}
+            >
+              +
+            </button>
+            <button
+              className="button start"
+              onClick={() => {
+                timerStart();
+              }}
+            >
+              start
+            </button>
+            <button
+              className="button down"
+              onClick={() => {
+                timerDecrease();
+              }}
+            >
+              -
+            </button>
+          </div>
+          <span className="timer-count">
+            {" "}
+            {timerMinute < 10 ? "0" + timerMinute : timerMinute} :{" "}
+            {timerSecond < 10 ? "0" + timerSecond : timerSecond}
+          </span>
         </div>
-            
-      </div>        
-    </section>        
-   )
-}             
-             
-
+      </div>
+    </section>
+  );
+};
 
 export default Timer;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import state from './state/state';
-// import timerState from "./state/timer-state";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
