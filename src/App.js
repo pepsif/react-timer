377e282a72ac5cc01576bf942c25fc3c485c83ec
pepsif <pepsi_fack@ukr.net>
2023-03-29 11:34:58 +0300

    feat: edit darktheme and add adaptive page for 300-500px

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
-import React from 'react';
-import './App.css';
-import Clock from './components/clock/clock';
-import StopWatchContainer from './components/stop-watch/StopWatchContainer';
-import Timer from './components/timer/timer';
-import {DarkThemeButton as ThemeButton} from "./components/DarkThemeButton/DarkThemeButton";
+import React, { useEffect } from "react";
+import "./App.css";
+import Clock from "./components/clock/clock";
+import StopWatchContainer from "./components/stop-watch/StopWatchContainer";
+import Timer from "./components/timer/timer";
+import { DarkThemeButton as ThemeButton } from "./components/DarkThemeButton/DarkThemeButton";
 import { useSelector } from "react-redux";
 
 function App() {
-const body = document.querySelector("body"); 
-const ligtOnOffValue = useSelector((state) => state.darkTheme.darkThemeOnOff);
-if(ligtOnOffValue === true ) {
-   
-  body.style.backgroundColor = "rgba(2,0,36, 0.6)"
-} else { body.style.backgroundColor = "rgba(196, 196, 196, 0.2)" }
+  
 
-console.log(ligtOnOffValue);
+  useEffect(() => {
+const body = document.querySelector("body");
+ const mainBlocks = document.querySelector(".clock-box");
+ const buttons = document.querySelectorAll("button")
 
-    return (
-        <div className="clock-box">
-            <ThemeButton />
-            <Clock />
-            <StopWatchContainer /> 
-            <Timer /> 
-        </div>
-    );
+ console.log(buttons)
+ 
+        //  --ON--
+    if (ligtOnOffValue === true) {
+      body.style.backgroundColor = "rgb(29 29 30 / 92%)";
+      body.style.color = "#908879fa";
+       for(let i=1; i<=3;i++) {
+        mainBlocks.children[i].style.boxShadow = "2px 2px 2px red";
+        
+       }
+       for(let i=0; i<buttons.length;i++) {
+        buttons[i].style.boxShadow = "23px 23px 46px #BEBEBE, -1px -1px 0px red";
+        buttons[i].style.backgroundColor = "#8e8678";
+       }
+       
+        // -OFF-
+    } else {
+      body.style.backgroundColor = "rgba(196, 196, 196, 0.2)";
+      body.style.color = "black";
+      for(let i=1; i<=3;i++) {
+      mainBlocks.children[i].style.boxShadow = '23px 23px 46px #BEBEBE, -23px -23px 46px #FFFFFF';
+
+      }
+      for(let i=0; i<buttons.length;i++) {
+        buttons[i].style.boxShadow = "5px 5px 10px #bcbcbc, -5px -5px 10px #ffffff";
+        buttons[i].style.backgroundColor="#E0E0E0"
+       }
+    }
+
+    // console.log(mainBlocks)
+  });
+
+  const ligtOnOffValue = useSelector((state) => state.darkTheme.darkThemeOnOff);
+
+  return (
+    <div className="clock-box">
+      <ThemeButton />
+      <Clock />
+      <StopWatchContainer />
+      <Timer />
+    </div>
+  );
 }
 
-export default App
-
+export default App;
